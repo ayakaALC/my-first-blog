d024a751a309a636f583517c5ad4bde783cedf50
Sang Soo <SangSoo.Ha@vaughan.ca>
2019-06-17 14:00:00 -0400

    hey

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os, dj_database_url
-
+import os
 
 LOGIN_REDIRECT_URL = '/'
 
