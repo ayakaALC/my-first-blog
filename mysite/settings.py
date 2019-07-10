5000e4f9330b218ca6b5d064ff3ca97bb13490da
Sang Soo <SangSoo.Ha@vaughan.ca>
2019-07-10 14:36:58 -0400

    SSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,15 +11,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-#import dj_database_url
+import dj_database_url
 
 LOGIN_REDIRECT_URL = '/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_ROOT = os.path.join(BASE_DIR,"media/images")
-MEDIA_URL = "/media/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
+MEDIA_URL = "media/images/"
 
 
 # Quick-start development settings - unsuitable for production
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'isThisReal',
+        'USER': 'has',
+        'PASSWORD': 'Egoist123!!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 #db_from_env = dj_database_url.config(conn_max_age=500)
 #DATABASES['default'].update(db_from_env)
